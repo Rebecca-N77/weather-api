e77f120950aad87f0d260667b53ae5d2eac97bf1
rebecca <rebeccanazha07@agmail.com>
2020-10-15 15:29:19 +0300

    React project had been done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import icon from "./img/weather-icons/clear.svg"
 import WeatherFirst from "./components/WeatherFIrst";
 import WeatherNext from "./components/Weathernext"
 import fakeWeatherData from "./fakeWeatherData.json";
+
 import "./App.css";
 //*http://api.openweathermap.org/data/2.5/forecast?q=London&cnt=8&units=metric&appid=f9f48aafa7cf62ac9dd58e05bbc814e8//
 class App extends Component {
@@ -11,35 +12,108 @@ class App extends Component {
     super(props);
     this.state = {
       name: "",
-      data: []
-      
+      data: [],
+      fdata: {},
+
     };
   }
-
+  
   handleInputChange = value => {
     let city =value
-    
+    let hours = []
     fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&cnt=8&units=metric&appid=f9f48aafa7cf62ac9dd58e05bbc814e8`)
     .then(res => res.json())
     .then(
       (result) => {
+        try {
+          let Adata = result.list
         
+        
+        console.log(Adata)
+        //weather first data manipulation
+        let Fdata = {
+          tempm : Adata[0].main.temp_min ,
+          tempx: Adata[0].main.temp_max ,
+          humidity: Adata[0].main.humidity ,
+          pressure: Adata[0].main.pressure ,
+          id: Adata[0].weather[0].id
+        }
+        let x = Fdata.id;
+        let color;
+        let navcolor;
+        switch(true){
+          case (x < 300):
+            navcolor = "#10171F"
+            color = "#F3AA4E"
+              break
+          case (x >= 300 && x <= 500):
+            navcolor = "#A8E9CA"
+            color = "##00203C"
+            break
+          case (x >= 500 && x <= 600):
+            navcolor = "#97BCCE"
+            color = "#0060AB"
+            break
+          case ( x >= 600 && x <= 700):
+           navcolor = "#A4A3CE"
+            color = "#717689"
+            
+            break
+          case (x >= 700 && x <= 800):
+            navcolor = "#67ebdb"
+            color = "#679e97"
+          
+            break
+          case (x === 800):
+            navcolor = "#FFFFFF"
+            color = "#F1B225"
+            break
+          case ( x === 801):
+            navcolor = "67ebdb"
+            color  = "#90CBFD"
+            break
+          case ( x >= 801 && x <= 805):
+            navcolor = "#F7BF37"
+            color  = "#003163"
+           break 
+        }
+        this.setState({
+          navcolor: navcolor,
+          color: color
 
-
-
-        console.log(result)
+        })
+        //weather next data manipulation
+       Adata.map(x => {
+        let d = new Date(x.dt_txt)
+      
+        let data = {
+          tempm : x.main.temp_min ,
+          tempx: x.main.temp_max ,
+          humidity: x.main.humidity ,
+          pressure: x.main.pressure ,
+          id: x.weather[0].id,
+          time: d.getUTCHours()
+        }
+        hours.push(data)
+       })
+       // setting the manipulated data to the state over her
         this.setState({
           isLoaded: true,
           dataFirst: result.list[0],
-          dataFinal: result.list
-
+          dataFinal: result.list,
+          fdata: Fdata,
+          mdata: hours
         });
+        } catch (error) {
+          alert(result.message)
+        }
+        
       },
       // Note: it's important to handle errors here
       // instead of a catch() block so that we don't swallow
       // exceptions from actual bugs in components.
       (error) => {
-        console.log(error)
+       alert(error.message)
         this.setState({
           isLoaded: true,
           error
@@ -51,13 +125,21 @@ class App extends Component {
 
   render() {
     return (
-      <div className="app">
+      <div style={{backgroundColor: this.state.color}} className="app">
         
-        <Search handleInput={this.handleInputChange} />
+        <Search  navcolor={this.state.navcolor} handleInput={this.handleInputChange} />
         
-        <img src={icon}></img>
-        <WeatherFirst />
-        <WeatherNext color="black" name={this.state.name} />
+        <div className="maincontainer" style={{backgroundColor: this.state.color}}>
+        <WeatherFirst color={this.state.color} data = {this.state.fdata}/>
+       
+
+        <WeatherNext color={this.state.color}  data = {this.state.mdata} />
+        
+        
+          
+        </div>
+        
+        
       </div>
     );
   }
--- src/components/Search.js
+++ src/components/Search.js
@@ -6,16 +6,32 @@ class Search extends React.Component {
   state = {
     input: ""
   };
-
+  _handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      this.props.handleInput(this.state.input);
+      
+    }
+  }
   render() {
+    
     return (
-      <div className="navbar">
+      <div style={{backgroundColor: this.props.navcolor}} className="navbar">
         
         <input className = "search"
           type="text"
           id="input-name"
-          onChange={event => {
-            this.setState({ input: event.target.value });
+          placeholder="Type your search here"
+          onChange={event =>  {
+            
+           this.setState({ input: event.target.value });
+
+          }}
+          onKeyDown={e => {
+
+            if (e.key === "Enter") {
+
+              this._handleKeyDown(e)
+            }
           }}
         />
         <button
